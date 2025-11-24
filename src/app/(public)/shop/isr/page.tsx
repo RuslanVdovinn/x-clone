530f265c1bfbfe0953fa33b3084144610e574e52
Ruslan Vdovin <vdovin@mgalaxy.pro>
2025-11-24 11:33:21 +0700

    SSR + ISR

--- src/app/(public)/shop/isr/page.tsx
+++ src/app/(public)/shop/isr/page.tsx
@@ -1,3 +1,17 @@
-export default function ShopISRPage() {
-    return <div>ISR</div>
+import { type IProduct, ProductCard } from "@/components/ProductCard"
+import { API_URL } from "@/constants"
+
+export default async function ShopISRPage() {
+    const response = await fetch(API_URL, { next: { revalidate: 5 } })
+    const products = (await response.json()) as IProduct[]
+    return (
+        <div className="grid grid-cols-2 gap-4">
+            {products.map(product => (
+                <ProductCard
+                    key={product.id}
+                    {...product}
+                />
+            ))}
+        </div>
+    )
 }
--- src/app/(public)/shop/page.tsx
+++ src/app/(public)/shop/page.tsx
@@ -1,5 +1,18 @@
+import { ProductCard, type IProduct } from "@/components/ProductCard"
+import { API_URL } from "@/constants"
 
 
-export default function ShopPage() {
-    return <div>SSR</div>
+export default async function ShopPage() {
+    const response = await fetch(API_URL, { cache: 'no-store' })
+    const products = (await response.json()) as IProduct[]
+    return (
+        <div className="grid grid-cols-2 gap-4">
+            {products.map(product => (
+                <ProductCard
+                    key={product.id}
+                    {...product}
+                />
+            ))}
+        </div>
+    )
 }
